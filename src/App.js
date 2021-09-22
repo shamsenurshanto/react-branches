dfa1bfbcb56001fe93017650f38c072bc842946c
shamsenurshanto <180104128@aust.edu>
2021-09-22 14:21:00 +0600

    added

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Person>
+
+     </Person>
     </div>
   );
 }
+ function Person()
+ {
+   const person={
+    backgroundColor: "goldenrod",
+    color:"#ffffff",
+    width: "50%"
+   }
+   return (  
 
+     <div style={person} >
+       <h1>this is </h1>
+     </div>
+   );
+ }
 export default App;
