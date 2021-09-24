6e2dd8b4302b2130fdf0bc4ae29cf2d8d50be7bd
shamsenurshanto <shamsenurshanto008@gmail.com>
2021-09-24 09:32:22 +0600

    useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   
+
   return (
    
     <div className="App">
-    
+     
+      <Countries>
+
+      </Countries>
+   
     </div>
   );
 }
- function Person()
- {
-   const person={
-    backgroundColor: "goldenrod",
-    color:"#ffffff",
-    width: "52%"
-   }
+ function Countries()
+ { 
+ 
+  const [country,setCountry] = useState([]);
+  useEffect(()=>{
+   
+ fetch('https://jsonplaceholder.typicode.com/users')
+ .then(response => response.json())
+ .then(data =>console.log(setCountry(data))
+ )
+
+
+  },[]);
+ 
+  
+  // const b ;
+  
    return (  
 
-     <div style={person} >
-       <h1>this is </h1>
-     </div>
+    <div>
+       <h4>
+    The countries are {country.length}
+    {
+    country.map(cot => <Country name={cot.name}></Country> )
+  }
+ 
+ </h4>
+ 
+    </div>
    );
  }
+ function Country(props){
+  
+  //  const ar = {country.map(cot=>{
+  //   // console.log(cot.name);
+  //   {cot.name}
+  // })}
+  console.log(props.name)
+  return(
+  
+    <div>
+      <h1>
+        Let's visit {props.name}
+       
+
+      </h1>
+    </div>
+  );
+}
 export default App;
